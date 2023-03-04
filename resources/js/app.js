7fd4ef6f38f9a0b5329d82c6e9327d76d68ecc22
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-03-04 19:35:15 +0100

    implements public channel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+Echo.channel('notifications')
+    .listen('UserSessionChanged',(e) =>{
+        console.log('hi');
+        let notificationElement = document.getElementById('notification')
+        console.log(notificationElement);
+        notificationElement.innerText = e.message;
+        notificationElement.classList.remove('invisible');
+        notificationElement.classList.remove('alert-success');
+        notificationElement.classList.remove('alert-danger');
+
+        notificationElement.classList.add('alert-' + e.type);
+
+    })
